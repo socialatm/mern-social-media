e013f31b2930196ab41f1b0fefc3f6e817b0d1f5
Ray <raypeaslee@gmail.com>
2024-10-31 11:21:06 -0400

    adds the navbar
    and all the functions we need

--- client/src/scenes/settingsPage/index.jsx
+++ client/src/scenes/settingsPage/index.jsx
@@ -1,5 +1,34 @@
+import { Box, useMediaQuery } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import Navbar from "scenes/navbar";
+
 const SettingsPage = () => {
-    return <h1>User Settings page</h1>
+    const [user, setUser] = useState(null);
+  const { userId } = useParams();
+  const token = useSelector((state) => state.token);
+
+  const getUser = async () => {
+    const response = await fetch(`http://localhost:3001/users/${userId}`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    setUser(data);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  if (!user) return null;
+    return (
+    <Box>
+      <Navbar /> 
+      <h1>User Settings page</h1>
+    </Box>
+    );
 }
 
 export default SettingsPage;
