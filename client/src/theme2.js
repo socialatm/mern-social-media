6578fe10499168fdf679672349ef68103f12f302
Ray <raypeaslee@gmail.com>
2024-11-04 11:01:20 -0500

    Update theme2.js
    spacing correstion

--- client/src/theme2.js
+++ client/src/theme2.js
@@ -77,27 +77,21 @@ export const themeSettings = (mode) => {
     typography: {
       fontSize: 12,
       h1: {
-        
         fontSize: 40,
       },
       h2: {
-        
         fontSize: 32,
       },
       h3: {
-        
         fontSize: 24,
       },
       h4: {
-        
         fontSize: 20,
       },
       h5: {
-        
         fontSize: 16,
       },
       h6: {
-        
         fontSize: 14,
       },
     },
