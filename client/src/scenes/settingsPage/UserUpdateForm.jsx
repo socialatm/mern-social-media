8822ecfb762930bc46e8486b79eb017bbf5f71ad
Ray <raypeaslee@gmail.com>
2024-11-04 21:51:12 -0500

    Update UserUpdateForm.jsx
    remove login fields

--- client/src/scenes/settingsPage/UserUpdateForm.jsx
+++ client/src/scenes/settingsPage/UserUpdateForm.jsx
@@ -197,28 +197,6 @@ const Form = () => {
                 </Box>
               </>
             )}
-
-            <TextField
-              label="Email"
-              onBlur={handleBlur}
-              onChange={handleChange}
-              value={values.email}
-              name="email"
-              error={Boolean(touched.email) && Boolean(errors.email)}
-              helperText={touched.email && errors.email}
-              sx={{ gridColumn: "span 4" }}
-            />
-            <TextField
-              label="Password"
-              type="password"
-              onBlur={handleBlur}
-              onChange={handleChange}
-              value={values.password}
-              name="password"
-              error={Boolean(touched.password) && Boolean(errors.password)}
-              helperText={touched.password && errors.password}
-              sx={{ gridColumn: "span 4" }}
-            />
           </Box>
 
           {/* BUTTONS */}
